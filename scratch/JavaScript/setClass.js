c0aee2abb10924b67bcebc783f9986a8eceb5add
Abdul Rahim <abdulrahim.alizada@powercoders.org>
2019-01-07 14:45:19 +0100

    Js file add
    -hide and show functions

--- scratch/JavaScript/setClass.js
+++ scratch/JavaScript/setClass.js
@@ -1,10 +1,42 @@
-console.log('console show my message!');    //Javascript code goes here
+/*console.log('console show my message!');    //Javascript code goes here
 
 function greet(name) {
   console.log('hello, ' + name);
+}*/
+
+function setClass(className) {
+  document.getElementById('mypara').className = className;
 }
 
-function setClass(event, className) {
+
+document.addEventListener('DOMContentLoaded', function (event) {
+
+  document.getElementById('make-warning').addEventListener('click', function (event) {
+    setClass('warning');
+  });
+
+ document.getElementById('make-tip').addEventListener('click', function (event) {
+    setClass('tip');
+  });
+
+  document.getElementById('make-normal').addEventListener('click', function (event) {
+    setClass('');
+  });
+});
+
+
+/*function makeTip() {
   let el = document.getElementById('mypara');
-  el.className = className;
+  el.className = 'tip';
 }
+
+function makeWarning() {
+  let el = document.getElementById('mypara');
+  el.className = 'warning';
+}
+
+function clearClass() {
+  let el = document.getElementById('mypara');
+  el.className = '';
+}*/
+
