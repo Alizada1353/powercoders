1fb25570a92c6a077d318fa86b7370d6971f2fc9
Abdul Rahim <abdulrahim.alizada@powercoders.org>
2019-01-08 16:58:32 +0100

    Changes on HTML Js & CSS
    -Delete button style.css
    - Delete button functionality
    -content wrapped with an id .html

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,19 +1,34 @@
 document.addEventListener('DOMContentLoaded', function (event) {
-    document.querySelector('button').addEventListener('click', function (event) {
-        console.log(document.getElementById('item').value);
-    });
+  document.querySelector('button').addEventListener('click', function (event) {
+
+    let inputBox = document.getElementById('item');
+    let li = createNewListItem(inputBox.value);
+
+    document.querySelector('ul').appendChild(li);
+  });
 });
 
+function createNewListItem(itemName) {
+  let li = document.createElement("li");
+
+  let span = document.createElement("span");
+  let spanText = document.createTextNode(itemName);
+
+  span.appendChild(spanText);
+  li.appendChild(span);
+
+  let button = document.createElement("button");
+  let buttonText = document.createTextNode('Delete');
+
+  button.addEventListener('click', function () {
+    li.remove();
+  });
+
+  button.appendChild(buttonText);
+  li.appendChild(button);
+
+  return li;
+}
 
 
 
-
-//this is just the more detailed way
-/*
-document.addEventListener('DOMContentLoaded', function (event) {
-    document.querySelector('button').addEventListener('click', function (event) {
-        let inputBox = document.getElementById('item');
-        console.log(inputBox.value);
-    });
-});
-*/
