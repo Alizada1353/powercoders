bd238c0e85db943465414465de8e9cbc2295eec5
Abdul Rahim <abdulrahim.alizada@powercoders.org>
2019-01-17 17:16:23 +0100

    Delete All Button -using arr forEach

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -1,48 +1,80 @@
 document.addEventListener('DOMContentLoaded', function (event) {
+  /** Widget that the user types an item in to. */
   const inputBox = document.getElementById('item');
   const shoppingList = document.querySelector('ul');
-  const addItemButton = document.querySelector('button');
+  const addIcon = document.querySelector('button');
+  const inputDiv = document.getElementById('inputs');
+  const DAB = document.createElement('button');
+  const DABText = document.createTextNode('Delete All');
+  DAB.appendChild(DABText);
+  inputDiv.appendChild(DAB).className = 'deleteAll';
 
-  addItemButton.addEventListener('click', function (event) {
-    if (inputBox.value.trim() !== '') {
-      shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
-      inputBox.value = '';
-      addItemButton.disabled = true;
+
+
+ DAB.addEventListener('click', function (event) {
+   const listItem = document.querySelectorAll('li');
+    listItem.forEach(function (el) {
+      el.remove();
+      inputBox.focus();
+    });
+ });
+
+  addIcon.addEventListener('click', function (event) {
+    const trimmedValue = inputBox.value.trim();
+
+    if (trimmedValue === '') {
+      inputBox.focus();
+      return;
+
     }
+    shoppingList.appendChild(createNewListItem(trimmedValue));
+    inputBox.value = '';
+    addIcon.disabled = true;
     inputBox.focus();
   });
 
-  inputBox.addEventListener('keyup', function (event) {
-    if (inputBox.value.trim() !== '') {
-      addItemButton.disabled = false;
-      if (event.key === 'Enter') {
-        shoppingList.appendChild(createNewListItem(inputBox.value.trim()));
-        inputBox.value = '';
-      }
+  document.querySelector('input').addEventListener('keyup', function (event) {
+    const trimmedValue = inputBox.value.trim();
+    addIcon.disabled = trimmedValue === '';
+
+    if (trimmedValue === '') {
+      return;
     }
-    if (inputBox.value.trim() === '') {
-      addItemButton.disabled = true;
+
+    if (event.key !== 'Enter') {
+      return;
     }
+  shoppingList.appendChild(createNewListItem(trimmedValue));
+  inputBox.value = '';
+  addIcon.disabled = true;
   });
   inputBox.focus();
-  addItemButton.disabled = true;
 });
 
+/**
+ * creates and returns an 'li' element for inclusion in the shopping list.
+ *
+ * @param {string} itemName Name of the item to add to the list
+ * @returns {HTMLElement} li element
+ */
 function createNewListItem(itemName) {
   const li = document.createElement("li");
-
   const span = document.createElement("span");
+
   li.appendChild(span).innerText = itemName;
 
-  const button = document.createElement("button");
-  li.appendChild(button).innerText = 'Delete';
+  const deleteIcon = document.createElement('i');
+  li.appendChild(deleteIcon).className = 'fa fa-trash';
 
-  button.addEventListener('click', function () {
+  deleteIcon.addEventListener('click', function (event) {
     li.remove();
+
     const inputBox = document.getElementById('item');
     inputBox.focus();
   });
 
   return li;
 }
+
+
 
