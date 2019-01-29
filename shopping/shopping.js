30ec6d39a1382bb8d616b90fa3ba7184559ac68f
Abdul Rahim <abdulrahim.alizada@powercoders.org>
2019-01-29 16:52:34 +0100

    --- Almost Finalized ---- -CSS Styles changed -JS changed -New element add to HTML

--- shopping/shopping.js
+++ shopping/shopping.js
@@ -2,6 +2,7 @@ function domContentLoaded() {
   /** Widget that the user types an item in to. */
   const inputBox = document.getElementById('item');
   const shoppingList = document.querySelector('ul');
+  const quantityBox = document.getElementById('quantity');
   const addIcon = document.querySelector('button');
   const clearBtn = document.querySelector('#clear');
 
@@ -16,19 +17,21 @@ function domContentLoaded() {
 
   addIcon.addEventListener('click', function (event) {
     const trimmedValue = inputBox.value.trim();
+    const quantityTV = quantityBox.value.trim();
 
     if (trimmedValue === '') {
       return;
     }
-    shoppingList.appendChild(createNewListItem(trimmedValue));
+    shoppingList.appendChild(createNewListItem(trimmedValue, quantityTV));
     inputBox.value = '';
     inputBox.focus();
     addIcon.disabled = true;
     clearBtn.disabled = false;
   });
 
-  document.querySelector('input').addEventListener('keyup', function (event) {
+  inputBox.addEventListener('keyup', function (event) {
     const trimmedValue = inputBox.value.trim();
+    const quantityTV = quantityBox.value.trim();
     addIcon.disabled = trimmedValue === '';
 
     if (trimmedValue === '') {
@@ -37,13 +40,15 @@ function domContentLoaded() {
     if (event.key !== 'Enter') {
       return;
     }
-    shoppingList.appendChild(createNewListItem(trimmedValue));
+    shoppingList.appendChild(createNewListItem(trimmedValue, quantityTV));
     inputBox.value = '';
     addIcon.disabled = true;
     clearBtn.disabled = false;
+    quantityBox.focus();
+    quantityBox.value = '';
   });
-  inputBox.focus();
   clearBtn.disabled = true;
+  quantityBox.focus();
 }
 
 if (document.readyState === 'loading') {
@@ -58,12 +63,21 @@ if (document.readyState === 'loading') {
  * creates and returns an 'li' element for inclusion in the shopping list.
  *
  * @param {string} itemName Name of the item to add to the list
+ * @param {string} quantity Quantity of the item to append to the list
  * @returns {HTMLElement} li element
  */
-function createNewListItem(itemName) {
+function createNewListItem(itemName, quantity) {
   const li = document.createElement("li");
   const span = document.createElement("span");
   li.appendChild(span).innerText = itemName;
+
+  if (quantity !== '') {
+    li.appendChild(document.createTextNode(' '));
+    const  quantityText = document.createElement('span');
+    quantityText.className = 'quantityText';
+    quantityText.textContent = '('+ quantity +')';
+    li.appendChild(quantityText);
+  }
 
   const deleteIcon = document.createElement('i');
   li.appendChild(deleteIcon).className = 'fa fa-trash';
