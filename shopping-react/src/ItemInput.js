0122eba8999e210598c64f13d28402c905a71676
Abdul Rahim <abdulrahim.alizada@powercoders.org>
2019-02-25 15:18:34 +0100

    --- add input widgets--

---
+++ shopping-react/src/ItemInput.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class ItemInput extends Component{
+  render() {
+    return (
+        <div>
+          <label htmlFor="item"> Enter an new item: </label>
+          <input type="text" id="quantity" size="8" placeholder="quantity"/>
+          <input type="text" id="item" placeholder="Type something to buy"/>
+          <button id="add">Add item</button>
+        </div>
+    );
+  }
+}
+
+export default ItemInput;
