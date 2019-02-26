ce9f2de97a276649fcf89c47feec1d9403910385
Abdul Rahim <abdulrahim.alizada@powercoders.org>
2019-02-26 16:25:17 +0100

    --- onKeyUp Event of Enter key --  -styles add to add button

--- shopping-react/src/ItemInput.js
+++ shopping-react/src/ItemInput.js
@@ -10,7 +10,18 @@ class ItemInput extends Component{
 
     this.onItemChange = this.onItemChange.bind(this);
     this.onQuantityChange = this.onQuantityChange.bind(this);
+    this.onKeyUp = this.onKeyUp.bind(this);
   }
+  onKeyUp(event) {
+    if (this.state.item.trim() === '') {
+      return;
+    }
+    if (event.key !== 'Enter') {
+      return;
+    }
+    this.props.onAddItem(this.state.item.trim(), this.state.quantity.trim());
+  }
+
   onItemChange(event) {
     this.setState({item: event.target.value});
   }
@@ -22,9 +33,16 @@ class ItemInput extends Component{
     return (
         <div>
           <label htmlFor="item"> Type the item you want to buy </label>
-          <input onChange={this.onQuantityChange} type="text" id="quantity" size="8" placeholder="quantity"/>
-          <input onChange={this.onItemChange} type="text" id="item" placeholder="Type something to buy"/>
-          <button onClick={() => this.props.onAddItem(this.state.item.trim(), this.state.quantity.trim())}
+          <input onChange={this.onQuantityChange}
+                 onKeyUp={this.onKeyUp}
+                 type="text" id="quantity" size="8" placeholder="quantity"/>
+
+          <input onChange={this.onItemChange}
+                 onKeyUp={this.onKeyUp}
+                 type="text" id="item" placeholder="Type something to buy"/>
+
+          <button onClick={() => this.props.onAddItem(this.state.item.trim(),
+                  this.state.quantity.trim())}
                   disabled={this.state.item.trim() === ''}
                   id="add">Add item</button>
         </div>
